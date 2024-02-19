535ade5222f213e3eb459614158811d9d4add20a
chandini02 <chandini0703@gmail.com>
2024-02-19 12:57:12 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'counter';
+  count:number=0;
+  isDisabled = false;
+  isEven = "Even";
+
+  check(){
+    if(this.count%2==1){
+      this.isEven="odd"
+
+
+    }
+    else{
+      this.isEven="even"
+    }
+  }
+  incr(){
+    if(this.count>=20){
+      this.isDisabled=true
+    }
+    else{
+      this.count++;
+      this.check()
+    }
+    
+  }
+  decr(){
+    if(this.count<=0){
+      this.isDisabled=false
+
+    }
+    else{
+      this.count--;
+      this.check()
+    }
+  }
+  show(){
+    this.count=0;
+  }
 }
